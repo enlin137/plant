b7b7ee3a802d519720fc2aaead1da70b0cf072f8
zel <849230488@qq.com>
2016-12-30 18:07:37 +0800

    add file

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,31 @@
 
 $(document).ready(function () {
-	$('.banner').unslider();
+	var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    autoplay:3000,
+    autoplayDisableOnInteraction : false,
+    
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+  });        
 	//导航点击
 	$(".navbar-nav li").click(function () {
-		$(".navbar-nav li").removeClass('on');
-		$(this).css({'height':'auto'});
-		$(this).find('dl').css({'display':'block'});
+		var acitve = $(this).hasClass('active_nav');
+		if(!acitve){
+			$(".navbar-nav li").removeClass('active_nav');
+			$(this).addClass('active_nav');
+			$(".navbar-nav li").find('dl').css({'display':'none'});
+			$(this).find('dl').css({'display':'block'});
+		}
+		else{
+			$(this).removeClass('active_nav');
+			$(this).find('dl').css({'display':'none'});
+		}
+		
 	});
 })
